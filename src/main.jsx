9910ac1632733fd301486ff30a5a3d2dbf8d54b9
Jeff Lovell <jeff@lovellworks.com>
2024-01-25 21:54:34 -0500

    understanding JSX

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import Greeting from './Greeting.jsx'
 import './index.css'
 import Food from './Food.jsx'
+import TestTitle from './TestTitle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
     <Food />
+    <TestTitle />
   </React.StrictMode>,
 )
