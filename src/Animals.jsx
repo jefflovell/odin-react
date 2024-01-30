7437b7df154667782a2b371787635cb83aed61c5
Jeff Lovell <jeff@lovellworks.com>
2024-01-30 00:13:50 -0500

    better readability of basic list

--- src/Animals.jsx
+++ src/Animals.jsx
@@ -1,10 +1,11 @@
 function Animals() {
   const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+  const animalsList = animals.map( animal => <li key={animal}>{animal}</li>)
   return (
     <div>
       <h1>Animals: </h1>
       <ul>
-        {animals.map( animal => <li key = {animal}>{animal}</li>)}
+        {animalsList}
       </ul>
     </div>
   );
